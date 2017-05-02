115323d7b584b9976e822c61e696cac4acb94524
qqmecl <qq562554268@163.com>
2017-05-02 23:33:31 +0800

    flaskweb87

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,24 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.name.data).first()
+        if user is not None and user.password_verify(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.arg.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('you have been logged out.')
+    return redirect(url_for('main.index'))
