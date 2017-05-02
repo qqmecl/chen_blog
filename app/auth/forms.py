1befc2fc63b037658dfcb9157e4d4699678b6f8a
qqmecl <qq562554268@163.com>
2017-05-03 00:42:39 +0800

    flaskweb90

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(Form):
     email = StringField('Email', validators = [Required(), Length(1, 64), Email()])
     password = PasswordField('Password', validators = [Required()])
     remember_me = BooleanField('keep me logged in')
     submit = SubmitField('Log in')
+
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators = [Required(), Length(1, 64), Email()])
+    username = StringField('Usrname', validators = [Required(), Length(1, 64), 
+                           Regexp('^[A-Za-z][A-Za-z0-9._]*$', 0, 'Username must have only letters,' 
+                           'numbers, dots or underscores')])
+    password = PasswordField('Password', validators = [Required(), EqualTo('password2', 
+                             message = 'password must match.')])
+    password2 = PasswordField('Comfirm password', validators = [Required()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username = field.data).first():
+            raise ValidationError('Username already in use.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,17 +1,18 @@
 from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email = form.name.data).first()
-        if user is not None and user.password_verify(form.password.data):
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.arg.get('next') or url_for('main.index'))
+            return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form = form)
 
@@ -22,3 +23,16 @@ def logout():
     logout_user()
     flash('you have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form = form)
