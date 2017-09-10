1bd10d1a3fca060d78866c2a5c1ae8f88def1bc2
qqmecl <qq562554268@163.com>
2017-09-11 07:38:01 +0800

    deploy 3

--- manage.py
+++ manage.py
@@ -10,6 +10,14 @@ if os.environ.get('FLASK_COVERAGE'):
     import coverage
     COV = coverage.coverage(branch = True, include = 'app/*')
     COV.start()
+
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
